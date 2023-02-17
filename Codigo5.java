7e77f4df80e1ca938bb6ade4c4eee9b9e0b1a038
Yami Escalona <yamescalord@gmail.com>
2023-02-17 17:32:43 -0600

    codigo de otros 5

---
+++ Codigo5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; // Se importa la clase Scanner. 
+
+public class Codigo5 {
+
+	// Se utiliza el método nextLine() de Scanner
+	    Scanner s = new Scanner(System.in);
+	    System.out.print("Introduzca un número: ');
+	    String ni = s.nextLine();
+
+	    int c = ni;
+	    
+	    int afo = 0;
+	    int noAfo = 0;
+	    
+	    while (ni > 0) {
+		  int digito = (int)(ni % 10);
+	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
+			afo++;
+	      } else {
+			noAfo++;
+		  
+		  ni /= 10;
+	    }
+
+	    if (afo > noAfo) {
+	      System.out.prinln("El " + c + " es un número afortunado.");
+	    } else {
+	      System.out.println("El " + c + " no es un número afortunado.");
+	    }
+	    
+	  }
+	  
+	}
